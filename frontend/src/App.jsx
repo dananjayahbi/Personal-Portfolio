30c1b83bd59545913725867d6c67c8afac8dfeeb
djd <dananjayahbi@gmail.com>
2024-05-13 11:22:49 +0530

    refactor: update routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Test1 from './Pages/Tests/test1';
 
 const App = () => {
+  const location = useLocation();
+
   return (
-    <div>App</div>
+    <>
+      <Routes location={location}>
+        <Route path="*" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/test1" element={<Test1 />} />
+      </Routes>
+    </>
   )
 }
 
